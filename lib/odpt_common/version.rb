1570355a25cd37b4a24823864b59cc87ed8a81d1
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-27 10:17:08 +0900

    20150427_101707 Install Gem (0.1.10)

--- lib/odpt_common/version.rb
+++ lib/odpt_common/version.rb
@@ -1,3 +1,3 @@
 module OdptCommon
-  VERSION = "0.1.7"
+  VERSION = "0.1.10"
 end
--- spec/odpt_common_spec.rb
+++ spec/odpt_common_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
-require 'deployer'
+require 'deplo'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.1.7"
+version = "0.1.10"
 
 describe OdptCommon do
   it "has a version number \'#{ version }\'" do
     expect( ::OdptCommon::VERSION ).to eq( version )
-    expect( ::Deployer.version_check( ::OdptCommon::VERSION , spec_filename ) ).to eq( true )
+    expect( ::Deplo.version_check( ::OdptCommon::VERSION , spec_filename ) ).to eq( true )
   end
 end

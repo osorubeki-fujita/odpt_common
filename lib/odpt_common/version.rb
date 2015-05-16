89e5125b35d0caf675ba0834f9e8b7e9296acae2
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-16 20:37:43 +0900

    20150516_203739 Bug fix

--- lib/odpt_common/version.rb
+++ lib/odpt_common/version.rb
@@ -1,3 +1,3 @@
 module OdptCommon
-  VERSION = "0.1.16"
+  VERSION = "0.1.17"
 end
--- spec/odpt_common_spec.rb
+++ spec/odpt_common_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'deplo'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.1.16"
+version = "0.1.17"
 
 describe OdptCommon do
   it "has a version number \'#{ version }\'" do

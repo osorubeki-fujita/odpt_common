49a9a61c17e1e718bb2739b179e6f664cc001aa0
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-20 19:32:00 +0900

    20150720_193109 Bug fix - OdptCommon::Factory::Design::RailwayLine::CssClass

--- lib/odpt_common/factory/design/railway_line/css_class.rb
+++ lib/odpt_common/factory/design/railway_line/css_class.rb
@@ -5,19 +5,19 @@ class OdptCommon::Factory::Design::RailwayLine::CssClass < OdptCommon::Factory::
   end
 
   def to_s
-    if object.marunouchi_branch_line?
+    if @object.marunouchi_branch_line?
       "tokyo_metro_marunouchi_branch"
-    elsif object.chiyoda_branch_line?
+    elsif @object.chiyoda_branch_line?
       "tokyo_metro_chiyoda_branch"
-    elsif object.shinkansen_of_jr_east?
+    elsif @object.shinkansen_of_jr_east?
       "shinkansen_e"
-    elsif shinkansen_of_jr_central?
+    elsif @object.shinkansen_of_jr_central?
       "shinkansen_c"
-    elsif minato_mirai_line?
+    elsif @object.minato_mirai_line?
       "yokohama_minatomirai_mm"
-    elsif keio_line?
+    elsif @object.keio_line?
       "keio_line"
-    elsif yurikamome_line?
+    elsif @object.yurikamome_line?
       "yurikamome_line"
     else
       super()

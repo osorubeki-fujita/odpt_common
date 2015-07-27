8976d924e61124b02de80818b4f6f9fa01a13178
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-27 22:40:22 +0900

    20150727_224016 Update - OdptCommon::Modules::Name::Common::RailwayLine::Info

--- lib/odpt_common/modules/name/common/railway_line/info.rb
+++ lib/odpt_common/modules/name/common/railway_line/info.rb
@@ -677,8 +677,8 @@ module OdptCommon::Modules::Name::Common::RailwayLine::Info
 
   # @!group 支線
 
-  def is_not_branch_railway_line_info?
-    !( is_branch_railway_line_info? )
+  def is_not_branch_railway_line_infos?
+    !( is_branch_railway_line_infos? )
   end
 
   # @!endgroup

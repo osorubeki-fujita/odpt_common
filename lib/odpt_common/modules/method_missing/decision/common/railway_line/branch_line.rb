7c32801902b02ee5bab6fc4ab24b93ac51eccc08
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-27 22:39:07 +0900

    20150727_223902 Update - OdptCommon::Modules::MethodMissing::Decision::Common::RailwayLine::BranchLine

--- lib/odpt_common/modules/method_missing/decision/common/railway_line/branch_line.rb
+++ lib/odpt_common/modules/method_missing/decision/common/railway_line/branch_line.rb
@@ -4,11 +4,11 @@ module OdptCommon::Modules::MethodMissing::Decision::Common::RailwayLine::Branch
 
   def method_missing( method_name , *args )
     if args.empty?
-      if /\A((?:is|is_not|not)_)?branch(?:_railway)?(?:_line)?(?:_info)?\?\Z/ =~ method_name.to_s
-        valid_method_name = "is_" + $1.to_s.gsub( /is_/ , "" ) + "branch_railway_line_info?"
+      if /\A((?:is|is_not|not)_)?branch(?:_railway)?(?:_line)?(?:_infos)?\?\Z/ =~ method_name.to_s
+        valid_method_name = "is_" + $1.to_s.gsub( /is_/ , "" ) + "branch_railway_line_infos?"
         return send( valid_method_name )
-      elsif /\Ahas_branch(?:_railway)?(?:_line)?(?:_info)?\?\Z/ =~ method_name.to_s
-        valid_method_name = "has_branch_railway_line_info?"
+      elsif /\Ahas_branch(?:_railway)?(?:_line)?(?:_infos)?\?\Z/ =~ method_name.to_s
+        valid_method_name = "has_branch_railway_line_infos?"
         return send( valid_method_name )
       end
     end
